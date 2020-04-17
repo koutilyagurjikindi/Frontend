26e427474c68022c57cece433da93d97a0309d44
koutilya <koutilya.g@flaashx.com>
2020-04-17 14:15:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -59,7 +59,6 @@ class App extends React.PureComponent {
   }
 }
 
-
 function mapStateToProps(state) {
   return { appName: state.common};
 }
@@ -78,129 +77,164 @@ export default connect(mapStateToProps, mapDispatchToProps)(App);
 //   render() {
 //     return (
 //       <div>
-//         <h1 title={"koutilya"}>Hello world</h1>
-//         <h2>Hello world</h2>
-//         <h3>Hello world</h3>
-//         <h4>Hello world</h4>
-//         <h5>Hello world</h5>
-//         <h6>Hello world</h6>
+//         <header>sachin</header>
+//         <nav>navigation</nav>
+//         <section>sachin is great</section>
+//         <article>koutilya</article>
+//         <aside>leela</aside>
+//         <footer>dachi</footer>
+//         <small>koutiklya is a fullstack developer </small>
 
-//         {/* Paragraph */}
-
-//         <p><a href={"https://www.google.com"} target={"_blank"}> koutilya</a> recently <strong>changed</strong> into full stack <em>developer</em></p>
-
-//         {/* Inline  */}
-        
-//         <strong>Koutilya</strong>
-//         <br/>
-//         <br/>
-
-//         <em>Koutilya</em>
-//         <br/>
-//         <br/>
-
-//         <a href="https://www.google.com">Koutilya</a>
-//         <br/>
-//         <br/>
-
-//           {/* List */}
-
-//           {/* unordered list */}
-//           <ul>
-//             <li >koutilya</li>
-//             <li>Sanjay</li>
-//             <li>Ravi</li>
-//             <li>Praneeth</li>
-//           </ul>
-
-//           {/* Order list */}
-//           <ol>
-//           <li >koutilya</li>
-//             <li>Sanjay</li>
-//             <li>Ravi</li>
-//             <li>Praneeth</li>
-//           </ol>
-
-//           {/* Table */}
-//           <table>
-//             <thead>
-//               <tr>
-//                 <th>Name</th>
-//                 <th>Email</th>
-//                 <th>Age</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//                 <tr>
-//                   <td>koutilya</td>
-//                   <td>koutilya56@gmail.com</td>
-//                   <td>45</td>
-//                 </tr>
-//                 <br></br>
-//                 <tr>
-//                   <td>Praneeth</td>
-//                   <td>Praneeth@gmail.com</td>
-//                   <td>55</td>
-//                 </tr>
-//                 <br></br>
-//                 <tr>
-//                   <td>Praneeth</td>
-//                   <td>Praneeth@gmail.com</td>
-//                   <td>55</td>
-//                 </tr>
-//             </tbody>
-//           </table>
-
-//           {/* Forms  */}
-//         <hr></hr>
-//           <form>
-//             <label>first</label>
-//             <input type="text" />
-//             <lable>Second</lable>
-//             <input value={"30"} type="text"/>
-//           </form>
-
-//           {/* Large Text area */}
-//           <div>
-//             <textarea name="message"/>
-//           </div>
-
-//           <div>
-//             <label>Gender</label>
-//             <select name="Gender">
-//               <option value={"Male"}>Male</option>
-//               <option value={"Female"}>Female</option>
-//             </select>
-//           </div>
-//           <div>
-//             <label>Age</label>
-//             <input value="30" type={"number"} name={"age"}/>
-//           </div>
-//           <div>
-//             <label>BirthDay</label>
-//             <input  type={"date"} name={"BirthDay"}/>
-//           </div>
-//           <input type="submit" name="submit" value="Submit"/>
-
-//           {/* Button */}
-
-//           <button>Sign Up</button>
-
-//           {/* Image */}
-//           <a href={"https://i.pinimg.com/474x/2e/2f/ac/2e2fac9d4a392456e511345021592dd2.jpg"}>
-//           <img width={"200px"} height={"100px"}  src={"https://i.pinimg.com/474x/2e/2f/ac/2e2fac9d4a392456e511345021592dd2.jpg"} alt="my Image"/>
-//           </a>
-
-//           {/* Quotation */}
-//           <blockquote cite={"https://www.google.com"}> Google com</blockquote>
-
-//           <p>this is <abbr title="koutilya is a full stack developer">koutilya</abbr></p>
-          
-//           <p>this is <cite>how the world becoming</cite> </p>
-//           <div style={{marginTop:"500px"}}></div>
+//         <details>keyboard typing is the best way learn blind folded</details>
+//         <main>this is koutiloya</main>
+//         <pre>this is not london</pre>
+//         <pre>this is not new york</pre>
+//         <b>hello</b>
+//         <strong>koutilyua</strong>
+//         <i>koutily a</i>
+//         <em>koutilya</em>
+//         <mark>koutiloya</mark>
+//         <small>koutilyua</small>
+//         <del>ko8utilya</del>
+//         <ins>koutilyua</ins>
+//         <sup>
+//           5<sub>10</sub>
+//         </sup>
+//         <q>hello worlds</q>
+//         <blockquote cite="http://www.worldwildlife.org/who/index.html">
+//           For 50 years, WWF has been protecting the future of nature. The
+//           world's leading conservation organization, WWF works in 100 countries
+//           and is supported by 1.2 million members in the United States and close
+//           to 5 million globally.
+//         </blockquote>
+//         <abbr title="World Health Organization">WHO</abbr> 
+//         <address>h.No : 6-1-889Box 564, Disneyland<br></br></address>
+//         <p><cite>The Scream</cite> by Edvard Munch. Painted in 1893.</p>
+//         <bdo dir="rtl">This text will be written from right to left</bdo>
 //       </div>
 //     );
 //   }
 // }
 
+// // <div>
+// //         <h1 title={"koutilya"}>Hello world</h1>
+// //         <h2>Hello world</h2>
+// //         <h3>Hello world</h3>
+// //         <h4>Hello world</h4>
+// //         <h5>Hello world</h5>
+// //         <h6>Hello world</h6>
 
+// //         {/* Paragraph */}
+
+// //         <p><a href={"https://www.google.com"} target={"_blank"}> koutilya</a> recently <strong>changed</strong> into full stack <em>developer</em></p>
+
+// //         {/* Inline  */}
+
+// //         <strong>Koutilya</strong>
+// //         <br/>
+// //         <br/>
+
+// //         <em>Koutilya</em>
+// //         <br/>
+// //         <br/>
+
+// //         <a href="https://www.google.com">Koutilya</a>
+// //         <br/>
+// //         <br/>
+
+// //           {/* List */}
+
+// //           {/* unordered list */}
+// //           <ul>
+// //             <li >koutilya</li>
+// //             <li>Sanjay</li>
+// //             <li>Ravi</li>
+// //             <li>Praneeth</li>
+// //           </ul>
+
+// //           {/* Order list */}
+// //           <ol>
+// //           <li >koutilya</li>
+// //             <li>Sanjay</li>
+// //             <li>Ravi</li>
+// //             <li>Praneeth</li>
+// //           </ol>
+
+// //           {/* Table */}
+// //           <table>
+// //             <thead>
+// //               <tr>
+// //                 <th>Name</th>
+// //                 <th>Email</th>
+// //                 <th>Age</th>
+// //               </tr>
+// //             </thead>
+// //             <tbody>
+// //                 <tr>
+// //                   <td>koutilya</td>
+// //                   <td>koutilya56@gmail.com</td>
+// //                   <td>45</td>
+// //                 </tr>
+// //                 <br></br>
+// //                 <tr>
+// //                   <td>Praneeth</td>
+// //                   <td>Praneeth@gmail.com</td>
+// //                   <td>55</td>
+// //                 </tr>
+// //                 <br></br>
+// //                 <tr>
+// //                   <td>Praneeth</td>
+// //                   <td>Praneeth@gmail.com</td>
+// //                   <td>55</td>
+// //                 </tr>
+// //             </tbody>
+// //           </table>
+
+// //           {/* Forms  */}
+// //         <hr></hr>
+// //           <form>
+// //             <label>first</label>
+// //             <input type="text" />
+// //             <lable>Second</lable>
+// //             <input value={"30"} type="text"/>
+// //           </form>
+
+// //           {/* Large Text area */}
+// //           <div>
+// //             <textarea name="message"/>
+// //           </div>
+
+// //           <div>
+// //             <label>Gender</label>
+// //             <select name="Gender">
+// //               <option value={"Male"}>Male</option>
+// //               <option value={"Female"}>Female</option>
+// //             </select>
+// //           </div>
+// //           <div>
+// //             <label>Age</label>
+// //             <input value="30" type={"number"} name={"age"}/>
+// //           </div>
+// //           <div>
+// //             <label>BirthDay</label>
+// //             <input  type={"date"} name={"BirthDay"}/>
+// //           </div>
+// //           <input type="submit" name="submit" value="Submit"/>
+
+// //           {/* Button */}
+
+// //           <button>Sign Up</button>
+
+// //           {/* Image */}
+// //           <a href={"https://i.pinimg.com/474x/2e/2f/ac/2e2fac9d4a392456e511345021592dd2.jpg"}>
+// //           <img width={"200px"} height={"100px"}  src={"https://i.pinimg.com/474x/2e/2f/ac/2e2fac9d4a392456e511345021592dd2.jpg"} alt="my Image"/>
+// //           </a>
+
+// //           {/* Quotation */}
+// //           <blockquote cite={"https://www.google.com"}> Google com</blockquote>
+
+// //           <p>this is <abbr title="koutilya is a full stack developer">koutilya</abbr></p>
+
+// //           <p>this is <cite>how the world becoming</cite> </p>
+// //           <div style={{marginTop:"500px"}}></div>
+// //       </div>
